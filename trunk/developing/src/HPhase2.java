89b4493fbc99d77c3476f474dcea50363e4c0435
virgilid <virgilid@dc64a182-3434-32eb-4c70-5d8940acd005>
2011-10-20 16:46:00 +0000

    Fase 2 da testare

--- trunk/developing/src/HPhase2.java
+++ trunk/developing/src/HPhase2.java
@@ -1,159 +1,84 @@
 
-
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.*; //Text
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 /**
- *
+ * 
  * @author virgilid
  */
-public class HPhase2 {
+public class HPhase2{
 
-	// OUTPUT VALUE MUST BE TEXT?
-    public static class MyMapper extends Mapper<LongWritable, Text, IntWritable, Text> {
-
-     
-        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException 
-        {           	
-        	String chunkName = ((FileSplit) context.getInputSplit()).getPath().getName();
-        	if(chunkName.startsWith("W"))
-        	{
-        		int i, row;
-        		for(i=0; i<chunkName.length() && 
-        					  chunkName.charAt(i)>='0' && chunkName.charAt(i)<='9'; i++);      		
-        		
-        		try
-        		{
-        			String rowNumber = chunkName.substring(i);
-        			row = new Integer(rowNumber);
-        		}
-        		catch(NumberFormatException e) { throw new IOException("Conversione nel nome del file fallita"); }
-        	
-        		//MatrixVector mv = new MatrixVector(value);
-            	context.write(new IntWritable(row), new Text("W"+value.toString())); // TO BE MODIFIED
-
-        	}
-        	else if (chunkName.startsWith("A"))
-        	{
-        		SparseElement se = new SparseElement(value);
-        		SparseVectorElement sve = new SparseVectorElement(se.getColumn(), se.getValue());
-            	context.write(new IntWritable(se.getRow()), new Text("A"+sve.toString())); // TO BE MODIFIED
-
-        	}
-        	else throw new IOException("The file to be analyzed is not the correct one");
-        }
-       
-    }
-
-    public static class MyReducer extends Reducer<IntWritable, Text, IntWritable, MatrixVector> {
-
-
-		private void scalarProductEmit(Double[] dValues, SparseVectorElement tmp, Context context) throws IOException, InterruptedException 
+	/* The output values must be text in order to distinguish the different data types */
+	public static class MyMapper extends Mapper<Text, Text, IntWritable, Text> {
+		
+		public void map(Text key, Text value, Context context) throws IOException, InterruptedException 
 		{
-
-			if(tmp.getValue() != 0.0)
-			{
-				Double[] doubleTmp = new Double[dValues.length];
-				
-				for(int j=0; j<doubleTmp.length; j++) 
-					doubleTmp[j] *= tmp.getValue();
-				
-				MatrixVector mvEmit = new MatrixVector(dValues.length, doubleTmp);
-				
-				context.write( new IntWritable(tmp.getCoordinate()), mvEmit);
-			}
+			int keyValue = Integer.parseInt(key.toString());
+			context.write(new IntWritable(keyValue), value); 
 		}
-    	
-    	public void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException 
-        {
-    		
-    		ArrayList<SparseVectorElement> arrList = new ArrayList<SparseVectorElement>();
-    		Iterator<Text> iter = values.iterator();
-    		
-    		Double[] dValues = null;
-    		
-    		Text val;
-    		while(iter.hasNext())
-    		{
-    			val = iter.next();
-    			
-            	if(val.charAt(0) == 'A') /// MULTIPLE A ELEMENTS VALUE ARE NOT TAKEN IN ACCOUNT
-            	{
-            		SparseVectorElement sve = SparseVectorElement.parseLine(val.toString().substring(1));
-            		arrList.add(sve);
-            	}
-            	else if(val.charAt(0) == 'W')
-            	{
-            		MatrixVector mv = MatrixVector.parseLine( val.toString().substring(1) );
-            		dValues = mv.getValues();
-            		
-            		for(SparseVectorElement tmp: arrList)
-            			scalarProductEmit(dValues, tmp, context);
-            		
-            		break;
-            	}
-    		}
-    		
-    		if(dValues != null)
-    		{
-    			while(iter.hasNext()) 
-    			{
-    				val = iter.next();
-    			
-    				if(val.charAt(0) == 'A') /// MULTIPLE A ELEMENTS VALUE ARE NOT TAKEN IN ACCOUNT
-    				{
-    					SparseVectorElement sve = SparseVectorElement.parseLine(val.toString().substring(1));
-    					scalarProductEmit(dValues, sve, context);
-    				}
-    			}
-    		}
-        }
 
+	}
+	
+	public static class MyReducer extends Reducer<IntWritable, Text, IntWritable, Text> {
 
-        
-    }
+		public void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException 
+		{	
+			/* The array contains the the row vector once the w row vector is read */
+			Double[] dValues = null, dResults = null;
+			MatrixVector mv = null;
 
+			Iterator<Text> iterator = values.iterator();
+			if(iterator.hasNext())
+			{
+				mv = new MatrixVector(iterator.next());
+				dResults = mv.getValues();
+			}
+			
+			while(iterator.hasNext())
+			{
+				mv = new MatrixVector(iterator.next());
+				dValues = mv.getValues();
+				for(int i=0; i<dValues.length; i++)
+				{
+					dResults[i] += dValues[i];
+				}
+			}
+			
+			MatrixVector result = new MatrixVector(dResults.length, dResults);
+			context.write(key, new Text(result.toString()));			}
+	}
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws Exception {
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) throws Exception 
+	{
+		Configuration conf = new Configuration();
 
-        Configuration conf = new Configuration();
+		Job job = new Job(conf, "MapRed Step1");
+		job.setJarByClass(HPhase1.class);
+		job.setMapperClass(MyMapper.class);
+		job.setReducerClass(MyReducer.class);
 
-        //int totalsize = Integer.parseInt(args[2]);
-        ///conf.setInt("matrix-mul-totalsize", totalsize); //the matrix is 'totalsize' by 'totalsize'
+		job.setOutputKeyClass(IntWritable.class);
+		job.setOutputValueClass(Text.class);
+				
+		//job.setOutputValueGroupingComparator(Class);
 
-        Job job = new Job(conf, "MapRed Step1");
-        job.setJarByClass(HPhase2.class);
-        job.setMapperClass(MyMapper.class);
-        job.setReducerClass(MyReducer.class);
-        //job.setNumReduceTasks(npart * npart);
+		TextInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-        
-//        job.setOutputKeyClass(SparseVectorElement.class);
-//        job.setOutputValueClass(IntWritable.class);
-
-        job.setOutputKeyClass(IntWritable.class);
-        job.setOutputValueClass(MatrixVector.class);
-
-        //FileInputFormat.addInputPath(job, new Path(args[0]));
-        TextInputFormat.addInputPath(job, new Path(args[0])); //need to read a complete line
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        
-        job.waitForCompletion(true);
-    }
+		job.waitForCompletion(true);
+	}
 }
