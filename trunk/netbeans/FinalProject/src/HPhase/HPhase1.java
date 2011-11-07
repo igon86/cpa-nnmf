ab0bfa02bf62a8c309a2b960f890b0e2789874cc
andre.lotta86 <andre.lotta86@dc64a182-3434-32eb-4c70-5d8940acd005>
2011-11-07 13:53:05 +0000

    progetto di netbeans caruccio caruccio

---
+++ trunk/netbeans/FinalProject/src/HPhase/HPhase1.java
@@ -0,0 +1,225 @@
+
+
+package HPhase;
+
+
+import java.io.IOException;
+import java.util.Iterator;
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DataInputBuffer;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.RawComparator;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import util.MatrixVector;
+import util.SparseElement;
+import util.SparseVectorElement;
+
+
+/**
+ *
+ * @author virgilid
+ */
+public class HPhase1 {
+
+	private static boolean W = false;
+	private static int currentRow;
+
+	/* The output values must be text in order to distinguish the different data types */
+	public static class MyMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+		protected void setup(Context context) throws IOException
+		{
+			String chunkName = ((FileSplit) context.getInputSplit()).getPath().getName();
+
+			/*  the number present in the file name is the number of the first stored row vector
+                            Through a static variable we take into account the right row number knowing that the
+                            row vector are read sequentially in the file split
+			*/
+
+			if (chunkName.startsWith("W")) /* A row vector must be emitted */
+			{
+				int i,j;
+
+				W = true;
+
+				for (i = 0; i < chunkName.length() &&
+					(chunkName.charAt(i) < '0' || chunkName.charAt(i) > '9'); i++);
+
+				for (j=i; j < chunkName.length() &&
+				 (chunkName.charAt(j) >= '0' && chunkName.charAt(j) <= '9'); j++);
+
+				try
+				{
+					String rowNumber = chunkName.substring(i, j);
+					System.out.println("GUARDARE:" + rowNumber);
+					currentRow = new Integer(rowNumber);
+				}
+				catch (NumberFormatException e) { throw new IOException("File name conversion failled"); }
+			}
+			else if( ! chunkName.startsWith("A")) throw new IOException("File name not correct");
+		}
+
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
+		{
+
+			if (W)
+			{
+				context.write(new Text("" + currentRow +"W"  ), new Text(value.toString()));
+				currentRow++;
+			}
+			else  /* The sparse element must be emitted */
+			{
+				SparseElement se = new SparseElement(value);
+				SparseVectorElement sve = new SparseVectorElement(se.getColumn(), se.getValue());
+				context.write(new Text("" + se.getRow() + "a"), new Text(sve.toString()));
+			}
+		}
+//lower case is usefull for the ordering of the key
+
+	}
+
+
+
+	  /**
+	   * Partition based on the first part of the pair.
+	   */
+	  public static class FirstPartitioner extends Partitioner<Text,Text>{
+	    @Override
+	    public int getPartition(Text key, Text value, int numPartitions) {
+	    	int j;
+	    	String s = key.toString();
+	    	for (j=0; j < s.length() && (s.charAt(j) >= '0' && s.charAt(j) <= '9'); j++);
+	    	int parsed = Integer.parseInt(s.substring(0, j));
+	      return parsed % numPartitions;
+	    }
+          }
+
+	  /**
+	   * Compare only the first part of the pair, so that reduce is called once
+	   * for each value of the first part.
+	   */
+	  public static class FirstGroupingComparator implements RawComparator<Text> {
+
+	    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2)
+	    {
+                DataInputBuffer buffer = new DataInputBuffer();
+                WritableComparable t1 = new Text();
+                WritableComparable t2 = new Text();
+                try {
+                    buffer.reset(b1, s1, l1);
+                    t1.readFields(buffer);
+                    System.out.println("STICAZZI1: "+t1.toString());
+                    buffer.reset(b2, s2, l2);
+                    t2.readFields(buffer);
+                    System.out.println("STICAZZI2: "+t2.toString());
+
+                } catch (IOException e) {
+                    System.out.println("col cazzo che ha funzionato");
+                    throw new RuntimeException(e);
+                }
+	    	return compare((Text )t1, (Text) t2);
+
+
+	    }
+
+
+	    public int compare(Text o1, Text o2)
+	    {
+
+	      String s1,s2;
+	      s1 = o1.toString().substring(0, o1.getLength()-1);
+              s2 = o2.toString().substring(0, o2.getLength()-1);
+              System.out.println("sono nella compare oggetti: "+s1+" VS "+s2);
+	      return s1.compareTo(s2);
+	    }
+	  }
+
+	public static class MyReducer extends Reducer<Text, Text, IntWritable, Text> {
+
+		private void scalarProductEmit(Double[] dValues, SparseVectorElement tmp, Context context) throws IOException, InterruptedException
+		{
+			if (tmp.getValue() != 0.0)
+			{
+				Double[] doubleTmp = dValues.clone();
+				for (int j = 0; j < doubleTmp.length; j++)
+				{
+					doubleTmp[j] *= tmp.getValue();
+				}
+
+				MatrixVector mvEmit = new MatrixVector(dValues.length, doubleTmp);
+
+				context.write(new IntWritable(tmp.getCoordinate()), new Text(mvEmit.toString()));
+			}
+		}
+
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException
+		{
+                        System.out.println("REDUCE KEY:" +key);
+			/* The array contains the the row vector once the w row vector is read */
+			Double[] dValues = null;
+
+			Text val;
+
+			Iterator<Text> iter = values.iterator();
+
+			if(iter.hasNext())
+			{
+				val = iter.next();
+				System.out.println("VALUE:"+val);
+
+				MatrixVector mv = MatrixVector.parseLine(val.toString());
+				dValues = mv.getValues();
+			}
+			else throw new IOException("It shouldn't be never verified");
+
+			while (iter.hasNext())
+			{
+				val = iter.next();
+
+				SparseVectorElement sve = SparseVectorElement.parseLine(val.toString());
+				scalarProductEmit(dValues, sve, context);
+			}
+		}
+	}
+
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) throws Exception
+	{
+		Configuration conf = new Configuration();
+
+		Job job = new Job(conf, "MapRed Step1");
+		job.setJarByClass(HPhase1.class);
+		job.setMapperClass(MyMapper.class);
+		job.setReducerClass(MyReducer.class);
+                //job.setNumReduceTasks(0);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+
+		job.setPartitionerClass(FirstPartitioner.class);
+		job.setGroupingComparatorClass(FirstGroupingComparator.class);
+
+		//job.setOutputValueGroupingComparator(Class);
+
+		TextInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.waitForCompletion(true);
+	}
+}
+
---
+++ trunk/netbeans/FinalProject/src/util/MatrixVector.java
@@ -0,0 +1,153 @@
+
+package util;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+
+
+public class MatrixVector implements WritableComparable<MatrixVector>{
+
+	private int elementsNumber;
+        protected Double[] value;
+
+        public MatrixVector(int element_number, Double[] elements)
+        {
+            this.elementsNumber = element_number;
+            this.value = elements;
+        }
+
+        public MatrixVector(Text s)
+        {
+        	parseLine(s.toString(), this);
+        }
+
+        public MatrixVector()
+        {
+        	;
+        }
+
+        static public MatrixVector parseLine(String s)
+        {
+        	MatrixVector mv = new MatrixVector();
+        	parseLine(s, mv);
+        	return mv;
+
+        }
+        // vector format : numbe_of_elements#elem1#elem2#elem3....
+        // a vector (row or column) per line
+        // no new line at the end of a file
+
+        static private void parseLine(String s, MatrixVector mv)
+        {
+
+        	try
+        	{
+        		String[] splitted = s.split("#");
+        		System.out.println("stringa partizionata");
+
+        		mv.elementsNumber = new Integer(splitted[0]);
+        		mv.value = new Double[mv.elementsNumber];
+        		System.out.println("Dimensione presa");
+
+        		for(int i=0; i<mv.elementsNumber && i<=splitted.length; i++)
+        		{
+        			mv.value[i] = new Double(splitted[i+1]);
+        			System.out.println("Ho acquisito il "+ i +"-esimo parametro");
+        		}
+        		System.out.println("Valori del vettore presi");
+        	}
+        	catch(NumberFormatException e)
+        	{
+        		System.out.println("Input Error reading SparseElement Value" + s);
+        		mv.elementsNumber = 0;
+        		mv.value = null;
+        	}
+        }
+
+		public int getNumberOfElement()
+        {
+            return this.elementsNumber;
+        }
+
+        public Double[] getValues()
+        {
+            return this.value;
+        }
+
+
+
+        public MatrixMatrix externalProduct(MatrixVector v) // Tensor Product
+        {
+        	int size = this.getNumberOfElement();
+        	if(this.getNumberOfElement() != v.getNumberOfElement()) return null;
+
+    		Double[][] tmp = new Double[size][size];
+			Double[] vect1 = this.getValues();
+			Double[] vect2 = v.getValues();
+
+			for(int i=0; i<size; i++)
+			{
+				for(int j=0; j<size; j++)
+				{
+					tmp[i][j] = vect1[i] * vect2[j];
+				}
+			}
+
+			return new MatrixMatrix(size, size, tmp);
+        }
+
+        public double internalProduct(MatrixVector v){
+            if (this.elementsNumber != v.elementsNumber) return 0;
+            int ret = 0;
+            for (int i =0;i<this.elementsNumber;i++){
+                ret += (this.value[i] * v.value[i]);
+            }
+            return ret;
+        }
+
+
+
+        @Override
+		public void readFields(DataInput arg0) throws IOException
+		{
+        	String tmp = arg0.readLine();
+        	parseLine(tmp);
+		}
+
+		@Override
+		public void write(DataOutput arg0) throws IOException
+		{
+			arg0.writeBytes(this.toString());
+		}
+
+		@Override
+		public int compareTo(MatrixVector o)
+		{
+			if(this.elementsNumber - o.elementsNumber != 0)
+				return this.elementsNumber - o.elementsNumber;
+			/*
+			int i;
+			for(i=0; i<this.elementsNumber; i++)
+				if(this.value[i] - o.value[i] != 0)
+					return (int) (this.value[i] - o.value[i]);
+			*/
+			return 0;
+		}
+
+		public String toString()
+		{
+			String tmp = ""+ this.elementsNumber;
+			StringBuilder stringBuilder = new StringBuilder(tmp);
+
+			for(int i=0; i<this.elementsNumber; i++)
+				stringBuilder.append("#" + this.value[i]);
+
+			stringBuilder.append('\n');
+			return stringBuilder.toString();
+		}
+}
+
