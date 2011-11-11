e7e68e69ec34dc0c143af1c672c2fee2c245de4a
virgilid <virgilid@dc64a182-3434-32eb-4c70-5d8940acd005>
2011-11-11 12:09:09 +0000

    ExternalPhase Package added

---
+++ netbeans/FinalProject/src/ExternalPhase/TextToSequenceMatrixWHTranslator.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ExternalPhase;
+
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import util.MatrixVector;
+
+
+public class TextToSequenceMatrixWHTranslator
+{
+
+	/* The output values must be text in order to distinguish the different data types */
+	public static class MyMapper extends Mapper<LongWritable, Text, IntWritable, MatrixVector> {
+
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
+		{
+			String[] values = value.toString().split("\t");
+			Integer index = new Integer(values[0]);
+			context.write(new IntWritable(index), MatrixVector.parseLine(values[1]));
+		}
+	}
+
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) throws Exception
+	{
+		if(args.length != 3)
+		{
+			System.err.println("The number of the input parameter are not corrected");
+			System.err.println("First Parameter: A/W files directories");
+			System.err.println("Second Parameter: Output directory");
+			System.exit(-1);
+		}
+
+		Configuration conf = new Configuration();
+
+		Job job = new Job(conf, "Translator from Text to Sequence for the H/W Matrix");
+		job.setJarByClass(TextToSequenceMatrixWHTranslator.class);
+		job.setMapperClass(MyMapper.class);
+
+		job.setNumReduceTasks(0);
+
+		job.setOutputKeyClass(IntWritable.class);
+		job.setOutputValueClass(MatrixVector.class);
+
+		job.setOutputFormatClass(SequenceFileOutputFormat.class);
+
+		TextInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.waitForCompletion(true);
+	}
+}
---
+++ netbeans/FinalProject/src/ExternalPhase/TextToSequenceSparseElementTranslator.java
@@ -0,0 +1,64 @@
+
+package ExternalPhase;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import util.SparseElement;
+import util.SparseVectorElement;
+
+public class TextToSequenceSparseElementTranslator
+{
+	public static class MyMapper extends Mapper<LongWritable, Text, IntWritable, SparseVectorElement> {
+
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
+		{
+			SparseElement se = SparseElement.parseLine(value.toString());
+			SparseVectorElement sve = new SparseVectorElement(se.getRow(), se.getValue());
+			context.write(new IntWritable(se.getRow()), sve);
+		}
+	}
+
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) throws Exception
+	{
+		if(args.length != 3)
+		{
+			System.err.println("The number of the input parameter are not corrected");
+			System.err.println("First Parameter: A/W files directories");
+			System.err.println("Second Parameter: Output directory");
+			System.exit(-1);
+		}
+
+		Configuration conf = new Configuration();
+
+		Job job = new Job(conf, "Translator from Text to Sequence for the Sparse Element");
+		job.setJarByClass(TextToSequenceSparseElementTranslator.class);
+		job.setMapperClass(MyMapper.class);
+
+		job.setNumReduceTasks(0);
+
+		job.setOutputKeyClass(IntWritable.class);
+		job.setOutputValueClass(SparseVectorElement.class);
+
+		job.setOutputFormatClass(SequenceFileOutputFormat.class);
+
+		TextInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.waitForCompletion(true);
+	}
+}
