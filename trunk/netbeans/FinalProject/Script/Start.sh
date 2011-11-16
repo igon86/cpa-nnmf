6483925a7d2c05995da1e8bf35b4c463f11c1da7
virgilid <virgilid@dc64a182-3434-32eb-4c70-5d8940acd005>
2011-11-16 16:52:22 +0000

    Script Tested

--- trunk/netbeans/FinalProject/Script/Start.sh
+++ trunk/netbeans/FinalProject/Script/Start.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-DEFAULT_LOCAL_A=${NMF_HOME}/Data/A
-DEFAULT_LOCAL_H=${NMF_HOME}/Data/H
-DEFAULT_LOCAL_W=${NMF_HOME}/Data/W
-DEFAULT_REMOTE_A=A
-DEFAULT_REMOTE_H=H0
-DEFAULT_REMOTE_W=W0
+${NMF_HOME}/Script/EraseAll.sh
 
 ${HADOOP_HOME}/bin/start-all.sh
 
-./EraseAll.sh
-
-./LoadData.sh ${DEFAULT_LOCAL_A} ${DEFAULT_REMOTE_A} ${DEFAULT_LOCAL_H} ${DEFAULT_REMOTE_H} ${DEFAULT_LOCAL_W} ${DEFAULT_REMOTE_W}
-
+echo ""
+echo "Wait until the web interface is fully up."
+echo "Check http://<hdfs_host>:50070/dfshealth.jsp"
+echo "Check http://<track_host>:50030/jobtracker.jsp"
