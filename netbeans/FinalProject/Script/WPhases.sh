5a16f8c9a844070ced3edc7729974732f16e6dca
virgilid <virgilid@dc64a182-3434-32eb-4c70-5d8940acd005>
2011-11-18 14:26:04 +0000

    Updated Script

--- netbeans/FinalProject/Script/WPhases.sh
+++ netbeans/FinalProject/Script/WPhases.sh
@@ -8,7 +8,7 @@
 
 ###### GENERIC VARIABLE ######
 JAR_NAME=$1
-accepted_parameters=5
+accepted_parameters=6
 ##printWorkingDir=$(pwd)
 
 ###### CHECKING PARAMETERS ######
@@ -20,29 +20,37 @@ then
 	echo "third parameter: H Files"
 	echo "fourth parameter: W Files"
 	echo "fiveth parameter: factorizing parameter"
+	echo "sixthth parameter: iteration number"
 	exit
 fi
 
 ###### DATA INPUT FILES ######
+iter_numb=$6
 A_DATA=$2
-H_DATA=$3
-W_DATA=$4
+H_DATA=${3}$((iter_numb-1))
+W_DATA=${4}$((iter_numb-1))
 K_DIM=$5
-X_PARTIAL=X_PARTIAL
-X_FINAL=X_FINAL
-C_DATA=C_DATA
-W_PRIME=W_PRIME
+X_PARTIAL=X_PARTIALw$6
+X_FINAL=X_FINALw$6
+Y_FINAL=Y_FINALw$6
+C_DATA=C_DATAw$6
+W_PRIME=W$((iter_numb++))
 
 
 ###### STARTING H PHASE ######
 
 ###### COMPUTING X ######
+echo %%%%PHASE 1%%%%%%
 ${HADOOP_HOME}/bin/hadoop jar ${JAR_NAME} WPhaseSequence.WPhase1 $A_DATA $H_DATA $X_PARTIAL $K_DIM
-${HADOOP_HOME}/bin/hadoop jar ${JAR_NAME} WPhaseSequence.WPhase2 $X_PARTIAL $X_FINAL $K_DIM
+echo %%%%PHASE 2%%%%%%
+${HADOOP_HOME}/bin/hadoop jar ${JAR_NAME} HPhaseSequence.HPhase2 $X_PARTIAL $X_FINAL $K_DIM
 
 ###### COMPUTING Y ######
-${HADOOP_HOME}/bin/hadoop jar ${JAR_NAME} WPhaseSequence.WPhase3 $H_DATA $C_DATA $K_DIM
+echo %%%%PHASE 3%%%%%%
+${HADOOP_HOME}/bin/hadoop jar ${JAR_NAME} HPhaseSequence.HPhase3 $H_DATA $C_DATA $K_DIM
+echo %%%%PHASE 4%%%%%%
 ${HADOOP_HOME}/bin/hadoop jar ${JAR_NAME} WPhaseSequence.WPhase4 $W_DATA $C_DATA $Y_FINAL $K_DIM
 
 ###### COMPUTING THE UPDATED MATRIX W ######
-${HADOOP_HOME}/bin/hadoop jar ${JAR_NAME} WPhaseSequence.WPhase5 $W_DATA $X_FINAL $Y_FINAL $W_PRIME $K_DIM
+echo %%%%PHASE 5%%%%%%
+${HADOOP_HOME}/bin/hadoop jar ${JAR_NAME} HPhaseSequence.HPhase5 $W_DATA $X_FINAL $Y_FINAL $W_PRIME $K_DIM
